0662629ce6718fd29dcdc9f32f17a75ceea18e93
y10ab1 <b07902144@ntu.edu.tw>
2021-01-05 05:42:59 +0800

    test

--- hw3_B06902136/receiver.cpp
+++ hw3_B06902136/receiver.cpp
@@ -203,7 +203,7 @@ int main(int argc, char *argv[])
         // copy a fream from buffer to the container on client
 
         memcpy(iptr + buffer_cnt, buffer, 32 * datasize); //Buffer 滿了就把他丟到frame裡面
-        iptr += 32 * datasize;
+        //iptr += 32 * datasize;
         //memcpy(iptr + imgSize / 4, buffer, 32 * datasize);
         buffer_cnt += 32 * datasize;
 
