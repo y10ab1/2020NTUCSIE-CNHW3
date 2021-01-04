8b62ff5d9f8ea1ccee5c6e48f42d22bd4a286a12
y10ab1 <b07902144@ntu.edu.tw>
2021-01-05 03:12:41 +0800

    test

--- hw3_B06902136/receiver.cpp
+++ hw3_B06902136/receiver.cpp
@@ -231,7 +231,7 @@ int main(int argc, char *argv[])
         memcpy(imgTemp.data, buffer, 32 * datasize);
         buffer_cnt++;
 
-        iptr += 32 * datasize;
+        //iptr += 32 * datasize;
         //memcpy(iptr, (const void *)'0', imgSize-sizeof(buffer));
         cout << "Temp mat size: " << imgTemp.total() * imgTemp.elemSize() << endl;
 
