ceaabb9ed6954fdf1e65b3a1e3437fb8ebda50d0
y10ab1 <b07902144@ntu.edu.tw>
2021-01-05 07:35:30 +0800

    test

--- hw3_B06902136/receiver.cpp
+++ hw3_B06902136/receiver.cpp
@@ -204,12 +204,12 @@ int main(int argc, char *argv[])
 
         if (buffer_cnt + datasize <= imgSize)
         {
-            for (int i = 0; i < 32; i++)
+            for (int i = 0; i < 32 && buffer_cnt + datasize <= imgSize; i++)
             {
                 memcpy(iptr + i * datasize, buffer + i * datasize, datasize); //Buffer 滿了就把他丟到frame裡面
+                buffer_cnt += (i + 1) * datasize;
             }
 
-            buffer_cnt += 32 * datasize;
             cout << "recv date : " << buffer_cnt << endl;
         }
         else
