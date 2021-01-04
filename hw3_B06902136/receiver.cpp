10887cd78db1539270a630a796693853ebdb428a
y10ab1 <b07902144@ntu.edu.tw>
2021-01-04 21:36:55 +0800

    update

--- hw3_B06902136/receiver.cpp
+++ hw3_B06902136/receiver.cpp
@@ -153,7 +153,7 @@ int main(int argc, char *argv[])
     int part_frame_cnt = 0;
 
     int leftSize = imgSize;
-    uchar *iptr=imgTemp.data;
+    
     while (1)
     {
         // get the size of a frame in bytes
@@ -225,7 +225,7 @@ int main(int argc, char *argv[])
         // copy a fream from buffer to the container on client
         //if (part_frame_cnt == 0)
 
-        iptr = imgTemp.data;
+        uchar *iptr=imgTemp.data;
         memcpy(iptr, buffer, sizeof(buffer));
         //part_frame_cnt += 31;
         //iptr += imgSize-sizeof(buffer);
