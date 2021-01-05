83bdaf98d2e4eba109494fec21be150d96caa13d
y10ab1 <b07902144@ntu.edu.tw>
2021-01-05 22:11:23 +0800

    OK

--- hw3_B06902136/sender.cpp
+++ hw3_B06902136/sender.cpp
@@ -249,7 +249,7 @@ int main(int argc, char *argv[])
             {
                 WinSize *= 2;
             }
-            else if (WinSize >= Threshold && WinSize <= 20 && Tout == 0)
+            else if (WinSize >= Threshold && WinSize < 20 && Tout == 0)
             {
                 WinSize += 1;
             }
